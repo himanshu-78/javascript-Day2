f668c3ee461feb8f28e1e7e68a86e71b1f4961d1
himanshu <himanshu01444@gmail.com>
2022-03-09 23:56:37 +0530

    initial commit

---
+++ day2.js
@@ -0,0 +1,92 @@
+// switch case
+
+// first type of scenario
+// we will compare the value of the variable directly
+
+// let a = 17;
+
+// switch (a){
+//     case 10:
+//         console.log("Value is 10");
+//         break;  //breaks the flow
+//     case 15:
+//         console.log("Value is 15");
+//         break;
+//     default:
+//         console.log("default case");
+//         break;
+// }
+
+// switch (a){
+//     case a>0:
+//         console.log("Positive");
+//         break;
+//     case a<0:
+//         console.log("Negative");
+//         break;
+//     default:
+//         console.log("Zero Value");
+//         break;
+//     }
+
+    //for loops
+   // let i=0;
+    //for( ; i<10  ;){
+       // console.log("value of i is",i);
+       // i++;
+    //}
+
+    //while loops
+    // let i=0;
+    // while(i<10){
+    //     console.log("value of i is", i);
+    //     i++;
+    // }
+    // console.log("outside")
+    
+    //do while loops
+    // let i=0;
+
+    // do{
+    //     console.log("value of i is", i);
+    //     i++;
+    // }while(i<10)
+
+    //nested loops
+
+    // for(let i=0;i<5;i++){
+    //     for(let j=0;j<2;j++){
+    //         console.log("value of is is:", i, "value of j is:", j);
+    //     }
+    // }
+
+
+    //functions
+    // let a=10;
+    // let b=20;
+    // function addtwonumbers(a,b){
+    //     console.log(a+b);
+    // }
+
+    // addtwonumbers(30,40);
+
+    //functions with return statement
+
+    // function addtwonumbers(x,y){
+    //     return x*y;
+    //     console.log("heeyyy")
+    // }
+    // const result=addtwonumbers(12,12);
+    // console.log(result);
+
+    //function which exist only in js
+
+    // const add =function(){
+    //     console.log("value");
+
+    // }
+     
+    // add();
+    
+
+
